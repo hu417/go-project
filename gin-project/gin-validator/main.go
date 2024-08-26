910dd42a7491ce56e8061e7e2fd26d2c4c6bcd95
hu417 <70016878+hu417@users.noreply.github.com>
2024-08-26 23:12:59 +0800

    Update main.go

--- gin-project/gin-validator/main.go
+++ gin-project/gin-validator/main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-	v1 "gin-validator/api/v1"
-	"gin-validator/internal/bootstrap"
-
 	"github.com/gin-gonic/gin"
+
+	v1 "gin-validator/api/v1"
+	"gin-validator/global"
+	"gin-validator/internal/bootstrap"
 )
 
 func main() {
 	// 初始化翻译器
-	bootstrap.InitTrans("zh")
+	trans, err := bootstrap.InitTrans("zh")
+	if err != nil {
+		panic(err)
+	}
+	global.Trans = trans
 
 	// 初始化路由
 	r := gin.Default()
 
 	r.GET("/ping", v1.Ping)
-	r.POST("/user/add", v1.AddUser)
+	r.POST("/user/signup", v1.Signup)
 	r.POST("/auth/login", v1.Login)
 
 	_ = r.Run(":8081")
