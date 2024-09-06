cc71ec6e0accb9676c6b8099c497ecdc92feaab4
hu417 <70016878+hu417@users.noreply.github.com>
2024-09-06 17:38:53 +0800

    Create limit.go

---
+++ gin/project/gin-api-demo/middleware/limit.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+  
+	"gin-api-demo/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DefaultLimit() gin.HandlerFunc {
+	// 默认每秒10个请求
+	return func(ctx *gin.Context) {
+		key := fmt.Sprintf("client::%v", ctx.ClientIP())
+		if err := utils.NewLimitConfig(key, 1, 10).SetLimitWithTime(); err != nil {
+			ctx.JSON(http.StatusTooManyRequests, gin.H{
+				"time": time.Now().Format("2006-01-02 15:04"),
+				"code": 429,
+				"msg":  err.Error(),
+			})
+			ctx.Abort()
+			return
+		} else {
+			ctx.Next()
+		}
+	}
+}
